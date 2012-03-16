08514bc2a90b68dbebbe24f145e4497f49c7acb6
kosteven <chunyuko85@gmail.com>
2012-03-16 14:20:57 +0800

    add comirva-0.36.jar

--- music_tag/src/sinica/music/tag/project/Music_tagActivity.java
+++ music_tag/src/sinica/music/tag/project/Music_tagActivity.java
@@ -9,6 +9,5 @@ public class Music_tagActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-//       test
     }
 }
