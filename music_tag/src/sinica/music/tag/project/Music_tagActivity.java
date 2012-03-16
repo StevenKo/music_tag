387a5b7cb191834d260b7aa336a9f49a3f7fcc3f
kosteven <chunyuko85@gmail.com>
2012-03-16 14:03:35 +0800

    first commit

---
+++ music_tag/src/sinica/music/tag/project/Music_tagActivity.java
@@ -0,0 +1,13 @@
+package sinica.music.tag.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Music_tagActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
